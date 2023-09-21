cedc279c7d42f643922e60c76695f6fd6dd91ebb
Jonathan Scagni <scagni1985@gmail.com>
2023-09-20 23:14:05 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,66 +1,78 @@
-const MAYOR_EDAD = 18
-let edad = +prompt ("Ingrese su edad:");
 
-//Condicional
+const listaProducto = []
 
-if (edad >= MAYOR_EDAD) {
-    alert ("Lista de bebidas" + "\n"
-           + "Vino 1500$" + "\n"
-           + "Cerveza 1000$" + "\n"
-           + "Licor 3000$" );
-} else {
-    alert ("Eres menor de edad, no puedes comprar bebidas alcohólicas.");
-    
-}
+listaProducto.push(new Producto("Vino Malbec", "Emilia", 1700));
+listaProducto.push(new Producto("Vino Cabernet", "Portillo", 1500));
+listaProducto.push(new Producto("Vino Sira", "EstibaI", 1300));
+listaProducto.push(new Producto ("Cerveza IPA","Andes", 1200));
+listaProducto.push(new Producto ("Cerveza ROJA","Imperial", 1100));
+listaProducto.push(new Producto ("Cerveza RUBIA","Andes", 1000));
+listaProducto.push(new Producto ("Fernet 750ml","Branca", 3750));
+listaProducto.push(new Producto ("Fernet 750ml","1882", 2550));
+listaProducto.push(new Producto ("Vodka","Smirnof", 2850));
+listaProducto.push(new Producto ("Vodka","SKY", 3950));
+listaProducto.push(new Producto ("Whisky","Johnnie Walker, Red Label", 11500));
+listaProducto.push(new Producto ("Whisky","Johnnie Walker, Black Label", 19500));
 
 
 
-const VALOR_VINO = 1500;
-const VALOR_CERVEZA = 1000;
-const VALOR_LICOR = 3000;
-const SI= "si";
-const NO= "no";
-let cantidadVino;
-let cantidadCerveza;
-let cantidadLicor;
-let totalVino;
-let totalCerveza;
-let totalLicor;
+const detalleFactura = []
+
+let salir;
+let cantidad;
+let opcion;
 let total = 0;
-let ticket = "NOMBRE    PRECIO   CANTIDAD     SUBTOTAL  \n"
-let subTotal;
-let modificar;
-// funcion
 
-function cargaCantidad (){
-    cantidadVino = +prompt ("Ingrese la cantidad de vinos que desea comprar")
-    cantidadCerveza = +prompt ("Ingrese la cantidad de cervezas que desea comprar")
-    cantidadLicor = +prompt ("Ingrese la cantidad de licores q desea comprar")  
-}
-//Ciclo
+// Menu de compras
+function menu () {
+
 do {
-    cargaCantidad ();
-    totalVino = VALOR_VINO * cantidadVino   
-    totalCerveza = VALOR_CERVEZA * cantidadCerveza   
-    totalLicor = VALOR_LICOR * cantidadLicor
-    modificar = prompt ("Desea modificar su compra: si o no")
-    while (modificar !== SI && modificar !== NO) {
-    modificar = prompt ("Su respuesta es incorrecta: responda si o no")
-    }
-    if (modificar == SI) {
-        cargaCantidad ();
-    }
+    const listaStringProductos = listaProducto.map((listaProducto,index) => 
 
-    subTotal = ` Vino             ${VALOR_VINO}          ${cantidadVino}              ${totalVino} \n
-    Cerveza           ${VALOR_CERVEZA}          ${cantidadCerveza}              ${totalCerveza} \n
-    Licor             ${VALOR_LICOR}            ${cantidadLicor}                 ${totalLicor}`
+    `${index + 1}  - ${listaProducto.nombre}  ${listaProducto.marca}   ${listaProducto.precio}`);
+
+     opcion = +prompt("Que producto desea elegir?" + "\n" + listaStringProductos.join("\n"))
+
+     while(opcion <= 0 || opcion > listaProducto.length || isNaN(opcion) || opcion == undefined) {
+     opcion = +prompt("Que producto desea elegir?" + "\n" + listaStringProductos.join("\n"))
+     }
+
+      cantidad = +prompt("Ingrese la cantidad que desea comprar:");
+
+     while(cantidad < 0 || isNaN(cantidad) || cantidad == undefined) {
+      cantidad = +prompt("Ingrese la cantidad que desea comprar:");
+     
+     }
+
+    detalleFactura.push(new itemFactura(listaProducto[opcion-1].nombre,listaProducto[opcion-1].precio,cantidad));
+
+    salir = prompt("Desea agregar otro producto? si/no");
     
-    total = total + totalVino + totalCerveza + totalLicor
 
-    alert (ticket + subTotal + "\n" 
-           + "total:" + total )
+}while(salir != "no") 
 
-}while (total <= 0)
+}
+
+alert("Bienvenido a nuestra tienda de bebeidas digital, presione aceptar para comenzar") 
+
+menu ();
+
+// ticket
+
+total = detalleFactura.reduce((acumulador,item) => acumulador + item.subtotal,0);
+
+const Ticket = detalleFactura.map((itemFactura,index,) => 
+
+`${index + 1}   ${itemFactura.nombre}    ${itemFactura.cantidad}      ${itemFactura.precio}        ${itemFactura.subtotal}`);
+
+alert( "PRODUCTO   CANTIDAD     PRECIO    SUBTOTAL" + "\n"
+     + Ticket.join("\n") + ` \n
+     El total de la compra es:  ${total}`);
+
+
+
+
+
 
 
 
---
+++ item.js
@@ -0,0 +1,9 @@
+class item {
+    producto;
+    cantidad;
+
+    contructor (producto,cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+}
